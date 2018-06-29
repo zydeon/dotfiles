f8fa4c4cd867c2dad1d28a9341857cea2aa1b257
Pedro Matias <pedro@pmatias.com>
2018-06-29 00:11:07 -0700

    [boostrap] Added mac boostrap file

---
+++ bootstrap/mac.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# 1 - Install brew
+
+# 2 - App brews
+brew cask install iterm2 \
+                  spotify \
+                  sublime-text \
+                  dropbox \
+                  spectacle \
+                  flux \
+                  skim \
+                  vlc \
+                  karabiner \
+                  seil \
+                  mactex
+
+# 3 - Normal brews
+brew install zsh \
+             tmux \
+             git \
+             vim \
+             cmake \
+             coreutils `# GNU ls has better color configs` \
+             python `# override MacOS python` \
+             python3 \
+             pypy3 \
+             wget \
+             ruby \
+             colordiff \
+             llvm  `# keep most updated clang` \
+             gdb \
+             `#conan # C++ package manager` \
+             tag
+
+
+# To maintain both clang versions
+# ln -s /usr/local/opt/llvm/bin/clang /usr/local/bin/clang6
+
+
+
+
+# Some quicklook plugins
+brew cask install qlcolorcode \
+                  qlstephen \
+                  qlmarkdown \
+                  quicklook-json \
+                  qlprettypatch \
+                  quicklook-csv \
+                  betterzipql \
+                  qlimagesize \
+                  webpquicklook \
+                  suspicious-package \
+                  quicklookase \
+                  qlvideo
+
+
+# Pip
+pip3 install bpython --user
+pip3 install gdbgui --user --upgrade
+
