1118bb9be91c6a977f41c0de78abe752b5796c3c
Pedro Matias <pedro@pmatias.com>
2019-07-13 20:15:52 -0700

    [tmux] Updated status bar

--- tmux/now_playing_artist.sh
+++ tmux/now_playing_artist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Source: https://www.tylerewing.co/tmux-now-playing 
 
-NOW_PLAYING=$(osascript <<EOF
+NOW_PLAYING_ARTIST=$(osascript <<EOF
 set spotify_state to false
 set itunes_state to false
 if is_app_running("Spotify") then
@@ -13,15 +13,13 @@ end if
 (* Whatever other music applications you use *)
 if spotify_state is equal to "playing" then
     tell application "Spotify"
-    set track_name to name of current track
     set artist_name to artist of current track
-    return track_name & " - #[bold]" & artist_name & "#[nobold]"
+    return artist_name
     end tell
 else if itunes_state is equal to "playing" then
     tell application "iTunes"
-    set track_name to name of current track
     set artist_name to artist of current track
-    return track_name & " - #[bold]" & artist_name & "#[nobold]"
+    return artist_name
     end tell
 else
     return ""
@@ -30,4 +28,4 @@ on is_app_running(app_name)
     tell application "System Events" to (name of processes) contains app_name
 end is_app_running
 EOF)
-echo $NOW_PLAYING
+echo $NOW_PLAYING_ARTIST
