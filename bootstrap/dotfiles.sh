0df8a9a5a5d192db261fdcd5573c6c37e0dda336
Pedro Matias <pedro@pmatias.com>
2019-09-30 18:44:29 -0700

    [boostrap] changes

---
+++ bootstrap/dotfiles.sh
@@ -0,0 +1,25 @@
+# Reset links
+rm -rf $HOME/.dir_colors
+rm -rf $HOME/.gdbinit
+rm -rf $HOME/.gitconfig
+rm -rf $HOME/.hushlogin
+rm -rf $HOME/.lldbinit
+rm -rf $HOME/.oh-my-zsh
+rm -rf $HOME/.vim
+rm -rf $HOME/.vimrc
+rm -rf $HOME/.zshrc
+rm -rf $HOME/.tmux
+rm -rf $HOME/.tmux.conf
+
+# Setup links
+ln -sf $HOME/.dotfiles/dir_colors $HOME/.dir_colors
+ln -sf $HOME/.dotfiles/gdb/gdbinit $HOME/.gdbinit
+ln -sf $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
+ln -sf $HOME/.dotfiles/.hushlogin $HOME/.hushlogin
+ln -sf $HOME/.dotfiles/lldb/lldbinit $HOME/.lldbinit
+ln -sf $HOME/.dotfiles/zsh/oh-my-zsh $HOME/.oh-my-zsh
+ln -sf $HOME/.dotfiles/vim $HOME/.vim
+ln -sf $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
+ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
+ln -sf $HOME/.dotfiles/tmux $HOME/.tmux
+ln -sf $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
