43caad6604728eb18e8d0c39ab605067f92c833e
Pedro Matias <zydeon@gmail.com>
2022-08-12 12:54:48 -0700

    Update setup.sh

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -1,2 +1,2 @@
-# To disable the Apple press and hold for VSCode only
+# To disable the Apple press and hold for VSCode only (requires restarting VSCode)
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false  
